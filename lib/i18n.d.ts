6bbf2e90c3a1c0dd8ade945ea4891471d686482d
JakDoh <jak.doh@gmail.com>
2026-09-07 21:52:23 +0200

    Expand Translation type with additional keys

--- lib/i18n.d.ts
+++ lib/i18n.d.ts
@@ -1,5 +1,5 @@
 export type Locale = "cs" | "en" | "de";
-export type Translation = Readonly<Record
+export type Translation = Readonly<Record<
   "driverAssistant" | "sourceDemo" | "sourceManual" | "truthNotConnected" | "truthShownData" | "truthDemo" | "truthManual" |
   "nextSafeDecision" | "inProgress" | "exceeded" | "pause" | "untilBreak" | "manualInput" | "continuousReference" | "why" |
   "currentActivity" | "manualHelper" | "onDevice" | "shiftOverview" | "timeOnePlace" | "adjust" | "continuousDrive" |
@@ -8,7 +8,35 @@ export type Translation = Readonly<Record
   "languageEn" | "languageDe" | "navCockpit" | "navLog" | "navDevice" | "navMore" | "activityDrive" | "activityDriveShort" |
   "activityWork" | "activityWorkShort" | "activityAvailable" | "activityAvailableShort" | "activityRest" | "activityRestShort" |
   "stopAndBreak" | "pauseInProgress" | "planBreak" | "modeChanged" | "continuousReset" | "officialSource" | "copyBetaReport" |
-  "protocolServiceDetected" | "protocolServiceMissing" | "reportCopied" | "reportCopyFailed",
+  "protocolServiceDetected" | "protocolServiceMissing" | "reportCopied" | "reportCopyFailed" |
+  "errNoBluetooth" | "errBleDisconnected" | "bleDisconnectedSafely" | "bleDeviceCancelled" |
+  "bleHandshakeFailed" | "bleConnectionFailed" | "bleTransportRejected" | "bleCreditTimeout" |
+  "bleTesterPresentTimeout" | "bleUdsSuccess" | "bleUdsRejected" | "bleUdsUnexpected" |
+  "bleSessionTimeout" | "bleSessionRejected" | "bleSessionUnexpected" | "bleCardReadSuccess" | "bleCardReadMissing" |
+  "bleRhmiError" | "bleDiagSessionError" | "bleAppProbeError" | "bleCardReadError" |
+  "msgInstallPrompt" | "msgInstalled" | "msgInstallCancelled" | "msgManualSaved" | "msgDemoNote" |
+  "msgDisclaimer" | "msgCompatCenterOpened" | "msgNetworkOnline" | "msgNetworkOffline" |
+  "logLocalLog" | "logTitle" | "logSubtitle" | "logTodayDrive" | "logActiveShift" |
+  "logEmptyTitle" | "logEmptyDesc" | "logNow" | "logManual" | "logDemo" | "logNote" |
+  "devCompatCenter" | "devTitle" | "devSubtitle" | "devProfileKicker" | "devProfileTitle" |
+  "devProfileDesc" | "devVehicle" | "devBus" | "devTruck" | "devOther" | "devTacho" |
+  "devModel" | "devModelPlaceholder" | "statusLinked" | "statusWaiting" | "statusUnsupported" |
+  "statusFailed" | "statusReady" | "statusUnknown" | "statusDevice" | "actionDisconnect" | "actionConnecting" |
+  "actionConnect" | "statAttempts" | "statDrops" | "statService" | "statTransport" | "statHandshake" |
+  "statUds" | "statCard" | "statYes" | "statNo" | "statRejected" | "chkWhatConfirmed" |
+  "chkBleTitle" | "chkBleDesc" | "chkConsentTitle" | "chkConsentDesc" | "chkServiceTitle" |
+  "chkServicePending" | "chkTransportTitle" | "chkTransportPass" | "chkTransportFail" |
+  "chkTransportPending" | "chkHandshakeTitle" | "chkHandshakePass" | "chkHandshakeRej" |
+  "chkHandshakeTime" | "chkHandshakeErr" | "chkHandshakeWait" | "chkHandshakePending" |
+  "chkUdsTitle" | "chkUdsPass" | "chkUdsRej" | "chkUdsTime" | "chkUdsUnexp" | "chkUdsErr" |
+  "chkUdsWait" | "chkUdsPending" | "chkSessionTitle" | "chkSessionPass" | "chkSessionTime" |
+  "chkSessionRej" | "chkSessionErr" | "chkSessionPending" | "chkCardTitle" | "chkCardPass" |
+  "chkCardFail" | "chkCardPending" | "chkFakeTitle" | "chkFakeDesc" | "stepsKicker" |
+  "step1" | "step2" | "step3" | "step4" | "setKicker" | "setAppInstalled" | "setAppInstalledDesc" |
+  "setInstallApp" | "setInstallAppDesc" | "setManualTime" | "setManualTimeDesc" | "setOfflineStat" |
+  "setOfflineOn" | "setOfflineOff" | "adKicker" | "adTitle" | "adDesc" | "aboutSource" |
+  "aboutManual" | "aboutCloud" | "aboutCloudDesc" | "provSources" | "modManualSrc" | "modTitle" |
+  "modDesc" | "modCancel" | "modSave" | "modHours" | "modMins" | "modContinuous" | "modDaily" | "modShift",
   string
 >>;
 export const supportedLocales: readonly Locale[];
